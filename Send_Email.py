40f015067043f5fa54719a8dad34588c36b09e58
AshutoshGunjal <43187740+AshutoshGunjal94@github.com>
2020-04-22 16:21:12 -0400

    Sending an email

---
+++ Send_Email.py
@@ -0,0 +1,24 @@
+import smtplib
+
+
+def Send_Email(subject, msg):
+    
+    try:
+
+        email_user = 'ashutoshgunjal1994@gmail.com'
+        email_send = 'ashutoshgunjal1994@gmail.com'
+        
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(email_user, '$$shreeguru$$')
+        message = 'Subject: {} \n\n {}'.format(subject, msg)
+        server.sendmail(email_user, email_send, message)
+        server.quite()
+        print("Success: Email Sent!")
+    
+    except:
+       print ("Email failed to send")
+
+subject = "Test Python Script"
+msg = "Python script successfully emailed"
